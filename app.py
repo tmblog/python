3fc3290e28694f1b3c1b94356d688773f05b51eb
tmblog <tmblog@users.noreply.github.com>
2023-07-06 21:36:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,88 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, render_template_string, request
 from flask import jsonify
 from flask_cors import CORS, cross_origin
+import sqlite3 as sql
 
 app = Flask(__name__)
 cors = CORS(app)
 
 @app.route('/')
 def home():
+   conn = sql.connect('database.db')
+   print("Opened database successfully")
+
+   conn.execute('CREATE TABLE IF NOT EXISTS students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
+   print("Table created successfully")
+   conn.close()
    return render_template('home.html')
 
-@app.route('/user')
-def get_user():
-    return jsonify(
-        username = "Ahmed",
-        email = "ahmed@test.com",
-        id = "00982"
+@app.route("/track/<id>")
+def get_parcel(id):
+   #take the id parameter and query the db with it
+   return jsonify(
+        idn = id,
+        nameOfDriver = "John",
+        status = "In transit",
+        eta = "23 minutes",
+        dispatchTime = "2pm",
+        address = "33 Pain Street, london S4 7GP"
     )
 
-@app.route('/car')
-def get_car():
-    return jsonify(
-        make = "Audi",
-        model = "A7",
-        style = "Black Edition"
-    )
+@app.get("/directions/<id>")
+def get_directions(id):
+    
+    #GET THE DIRECTION DATA FROM DATABASE
+    #FOR THE EXAMPLE I WILL JUST USE DUMMY DATA
+    directions = [{"field1": "field1 data"},
+                  {"field1": "field2 data"}]
+    
+    """
+        Any data you want to pass to the template you just
+        list it as a keyword argument 
+        e.g if you wanted to add a variable for user
+        you would just add user=user_object 
+    """
+    return  render_template("directions.html", 
+                            directions=directions)
 
-@app.route('/class')
-def get_class():
-    return jsonify(
-        name = "Y12 CS",
-        quality = "Kinda Poor now",
-        potential = "High"
-    )
+@app.route('/enternew')
+def new_student():
+   return render_template('student.html')
 
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+   if request.method == 'POST':
+      try:
+         nm = request.form['nm']
+         addr = request.form['add']
+         city = request.form['city']
+         pin = request.form['pin']
+         
+         with sql.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO students (name,addr,city,pin) VALUES (?,?,?,?)",(nm,addr,city,pin) )
+            
+            con.commit()
+            msg = "Record successfully added"
+      except:
+         con.rollback()
+         msg = "error in insert operation"
+      
+      finally:
+         return render_template("result.html",msg = msg)
+         con.close()
+
+@app.route('/list')
+def list():
+   con = sql.connect("database.db")
+   con.row_factory = sql.Row
+   
+   cur = con.cursor()
+   cur.execute("select * from students")
+   
+   rows = cur.fetchall(); 
+   return render_template("list.html",rows = rows)
 
 if __name__ == '__main__':
    app.run(debug=True)
